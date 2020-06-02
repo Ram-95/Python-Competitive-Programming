59848b92e8f7aa8ac82fdd5a51ff8174eca7b947
Ram Babu <ram.kottapally@gmail.com>
2020-06-02 13:34:52 +0530

    Create Day2_Delete_Node_in_LL.py

---
+++ Leetcode_30day_challenge/June_Challenge_2020/Day2_Delete_Node_in_LL.py
@@ -0,0 +1,23 @@
+'''
+Delete a given Node from a LL. We are not given the 
+head of LL. We just have access to the node 
+that must be deleted.
+
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+        
