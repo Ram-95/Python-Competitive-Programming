d841524ed5eeacdc84ad58b619755587071ab3d8
Ram Babu <ram.kottapally@gmail.com>
2020-06-04 12:56:28 +0530

    Create Day4_Reverse_String.py

---
+++ Leetcode_30day_challenge/June_Challenge_2020/Day4_Reverse_String.py
@@ -0,0 +1,19 @@
+#Reverse a string without using extra space.
+#Used Two pointer approach.
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        n = len(s)
+        #Two pointer Approach
+        (p1,p2) = (0,n-1)
+        while p1 <= p2:
+            s[p1], s[p2] = s[p2], s[p1]
+            p1 += 1
+            p2 -= 1
+            
+        
+        
+
