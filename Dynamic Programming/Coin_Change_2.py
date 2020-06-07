51c0e1969a02b63220470e263a94b4881193d6e6
Ram Babu <ram.kottapally@gmail.com>
2020-06-07 15:13:00 +0530

    Update Coin_Change_2.py

--- Dynamic Programming/Coin_Change_2.py
+++ Dynamic Programming/Coin_Change_2.py
@@ -1,3 +1,18 @@
+'''
+You are given coins of different denominations and a total amount of money.
+Write a function to compute the number of combinations that make up that amount. 
+You may assume that you have infinite number of each kind of coin.
+
+Input: amount = 5, coins = [1, 2, 5]
+Output: 4
+Explanation: there are four ways to make up the amount:
+5=5
+5=2+2+1
+5=2+1+1+1
+5=1+1+1+1+1
+
+'''
+
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         if amount == 0:
