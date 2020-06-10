f736d99b42e17ad77b79fa42866ef176651e6e19
Ram Babu <ram.kottapally@gmail.com>
2020-06-10 12:59:39 +0530

    Update Day10_Search_Insert_Position.py

--- Leetcode_30day_challenge/June_Challenge_2020/Day10_Search_Insert_Position.py
+++ Leetcode_30day_challenge/June_Challenge_2020/Day10_Search_Insert_Position.py
@@ -4,7 +4,8 @@ Given a sorted array and a target value, return the index if the target is found
 You may assume no duplicates in the array.
 '''
 
-#Method 1 - Time - O(N)
+#Method 1 - Linear - Time - O(N)
+
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         n = len(nums)
@@ -14,5 +15,21 @@ class Solution:
         
         #If the target is greater than max of array
         return n
-        
-        
+
+#Method 2 - Binary Search - Time - O(Log N)
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = (low + high)//2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+                
+        return low
+           
