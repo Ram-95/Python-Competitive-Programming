7e06b7ae3356983a8574c54d595a2e5405030f44
Ram Babu <ram.kottapally@gmail.com>
2020-06-15 12:51:41 +0530

    Create Day15_Search_in_BST.py

---
+++ Leetcode_30day_challenge/June_Challenge_2020/Day15_Search_in_BST.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root is None or val ==root.val:
+            return root
+        elif val < root.val:
+            return self.searchBST(root.left, val)
+        elif val > root.val:
+            return self.searchBST(root.right, val)
+        
