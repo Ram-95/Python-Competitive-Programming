52b772b7c178f1295c5ad28fac0a19ae3e744d73
Ram Babu <ram.kottapally@gmail.com>
2020-06-28 13:54:34 +0530

    Create Day-28_Reconstruct_Itinerary.py

---
+++ Leetcode_30day_challenge/June_Challenge_2020/Day-28_Reconstruct_Itinerary.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        d = collections.defaultdict(list)
+        
+        for i,j in sorted(tickets)[::-1]:
+            d[i] += j,
+        
+        ans = []    
+        def solve(a):
+            while d[a]:
+                solve(d[a].pop())
+                
+            ans.append(a)
+        solve("JFK")
+        
+        return ans[::-1]
+            
+        
+
+                
+            
+            
+        
