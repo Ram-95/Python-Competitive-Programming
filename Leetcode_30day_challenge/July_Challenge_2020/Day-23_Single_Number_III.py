2e080990ff1c74e06496230157d7806e72f30deb
Ram Babu <ram.kottapally@gmail.com>
2020-07-23 14:30:36 +0530

    Update Day-23_Single_Number_III.py

--- Leetcode_30day_challenge/July_Challenge_2020/Day-23_Single_Number_III.py
+++ Leetcode_30day_challenge/July_Challenge_2020/Day-23_Single_Number_III.py
@@ -17,3 +17,20 @@ class Solution:
         # Finally return the keys of the Dictionary
         return d.keys()
         
+
+'''
+Method 2 - Concept - O(N) Time and O(1) Space
+ 
+ - Let a and b be the two unique numbers
+- XORing all numbers gets you (a xor b)
+- (a xor b) must be non-zero otherwise they are equal
+- If bit_i in (a xor b) is 1, bit_i at a and b are different.
+- Find bit_i using the low bit formula m & -m
+- Partition the numbers into two groups: one group with bit_i == 1 and the other group with bit_i == 0.
+- a is in one group and b is in the other.
+- a is the only single number in its group.
+- b is also the only single number in its group.
+- XORing all numbers in a's group to get a
+- XORing all numbers in b's group to get b
+- Alternatively, XOR (a xor b) with a gets you b.
+'''
