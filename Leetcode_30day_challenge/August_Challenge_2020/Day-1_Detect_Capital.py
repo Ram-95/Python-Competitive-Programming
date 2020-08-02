4bf42d34e27a9959f72f28725ae09da6da9b2a6b
Ram Babu <ram.kottapally@gmail.com>
2020-08-02 22:21:21 +0530

    Update Day-1_Detect_Capital.py

--- Leetcode_30day_challenge/August_Challenge_2020/Day-1_Detect_Capital.py
+++ Leetcode_30day_challenge/August_Challenge_2020/Day-1_Detect_Capital.py
@@ -1,1 +1,5 @@
-
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word == word.title() or word == word.upper() or word == word.lower():
+            return True
+        return False
