98e8d84b102e8e14f3dea9c5b5bcb842f6784222
Ram Babu <ram.kottapally@gmail.com>
2020-08-12 13:27:07 +0530

    Update day-12_Pascal Triangle_II.py

--- Leetcode_30day_challenge/August_Challenge_2020/day-12_Pascal Triangle_II.py
+++ Leetcode_30day_challenge/August_Challenge_2020/day-12_Pascal Triangle_II.py
@@ -32,3 +32,16 @@ class Solution:
             arr.append(math.comb(rowIndex,i))
         
         return arr
+
+    
+# Using zip() function
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        row = [1]
+        for _ in range(rowIndex):
+            row = [x + y for x, y in zip([0]+row, row+[0])]
+        return row
