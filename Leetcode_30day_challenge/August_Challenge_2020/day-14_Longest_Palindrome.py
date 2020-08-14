b5801b92b972ce90b2d5ced579f17286916d9d79
Ram Babu <ram.kottapally@gmail.com>
2020-08-14 15:59:25 +0530

    Update day-14_Longest_Palindrome.py

--- Leetcode_30day_challenge/August_Challenge_2020/day-14_Longest_Palindrome.py
+++ Leetcode_30day_challenge/August_Challenge_2020/day-14_Longest_Palindrome.py
@@ -3,6 +3,7 @@ Q: Given a string which consists of lowercase or uppercase letters, find the len
 
 This is case sensitive, for example "Aa" is not considered a palindrome here.
 '''
+# Using Dictionary - O(N) Time
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         d = {}
@@ -12,10 +13,6 @@ class Solution:
             else:
                 d[i] += 1
         
-        #print(d)
-        arr = list(d.values())
-        arr.sort(reverse=True)
-        #print(arr)
         ans = 0
         '''
         Odd and Odd - Not possible (But the frequency - 1 will make a valid palindrome)
@@ -25,7 +22,7 @@ class Solution:
         Odd and Even - Possbile
         
         '''
-        for i in arr:
+        for i in d.values():
             if not(ans%2 == 1 and (i%2 == 1)):
                 ans += i
             else:
@@ -33,8 +30,25 @@ class Solution:
                     ans += i-1
         
         return ans
-                
-                
-            
-            
+ 
+#Using Counter
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        ans = 0
+        '''
+        Odd and Odd - Not possible (But the frequency - 1 will make a valid palindrome)
+    
+        Even and Odd - Possible
+        Even and Even - Possible
+        Odd and Even - Possbile
         
+        '''
+        for i in collections.Counter(s).values():
+            if not(ans%2 == 1 and (i%2 == 1)):
+                ans += i
+            else:
+                if i > 1:
+                    ans += i-1
+        
+        return ans
+ 
