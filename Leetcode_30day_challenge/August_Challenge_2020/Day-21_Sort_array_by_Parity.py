7ef4a49c6641ed6cd04dc9dbd0e478825a3d75ee
Ram Babu <ram.kottapally@gmail.com>
2020-08-21 14:51:25 +0530

    Update Day-21_Sort_array_by_Parity.py

--- Leetcode_30day_challenge/August_Challenge_2020/Day-21_Sort_array_by_Parity.py
+++ Leetcode_30day_challenge/August_Challenge_2020/Day-21_Sort_array_by_Parity.py
@@ -3,7 +3,7 @@ Q: Given an array A of non-negative integers, return an array consisting of all 
 
 You may return any answer array that satisfies this condition.
 '''
-
+# My Solution - O(N) Space
 class Solution:
     def sortArrayByParity(self, A: List[int]) -> List[int]:
         d = {'even': [], 'odd': []}
@@ -15,4 +15,21 @@ class Solution:
         
         ans = d['even'] + d['odd']
         return ans
-                
+
+    
+#Two Pointer - Inplace O(1) Space
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        #Two pointer approach
+        p1, p2 = 0, len(A)-1
+        while p1 < p2:
+            if A[p1] & 1 > A[p2] & 1:
+                # Swapping
+                A[p1], A[p2] = A[p2], A[p1]
+            
+            if A[p1] & 1 == 0:
+                p1 += 1
+            if A[p2] & 1 == 1:
+                p2 -= 1
+        
+        return A
