7e997e001192c8381a42282352691bc1baa88d0b
Ram Babu <ram.kottapally@gmail.com>
2020-08-26 14:30:35 +0530

    Update Day-10_Excel_Sheeet_Column_Number.py

--- Leetcode_30day_challenge/August_Challenge_2020/Day-10_Excel_Sheeet_Column_Number.py
+++ Leetcode_30day_challenge/August_Challenge_2020/Day-10_Excel_Sheeet_Column_Number.py
@@ -17,9 +17,40 @@ class Solution:
             ans += d[s[i+1]]
             #print(ans)
             
-        
-        
-        
         return ans
+    
+ 
+
+# Reverse of the above - Given Excel Column Number to Column Code
+# Eg: I/P: 28 O/P: "AB"
+
+# Excel Column Number to Column Code
+def titleToNumber(n):
+    d = {}
+    ans = ''
+    for i in range(65, 91):
+        d[i - 65 + 1] = chr(i)
+    #print(d)
+    temp = []
+    while n:
+        quo, rem = divmod(n, 26)
+        temp.insert(0,rem)
+        n = quo if rem != 0 else quo - 1
+
+    print(temp)
+    for i in temp:
+        if i != 0:
+            ans += d[i]
+        else:
+            ans += d[26]
+
+    return ans
+
+
+n = int(input())
+print(titleToNumber(n))
+
+
+
         
         
