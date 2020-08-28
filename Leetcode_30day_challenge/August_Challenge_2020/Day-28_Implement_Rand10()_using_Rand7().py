55b13cebd3807d5e249371247424c32c79d1ee44
Ram Babu <ram.kottapally@gmail.com>
2020-08-28 13:19:44 +0530

    Update Day-28_Implement_Rand10()_using_Rand7().py

--- Leetcode_30day_challenge/August_Challenge_2020/Day-28_Implement_Rand10()_using_Rand7().py
+++ Leetcode_30day_challenge/August_Challenge_2020/Day-28_Implement_Rand10()_using_Rand7().py
@@ -1,13 +1,11 @@
 # Q: https://leetcode.com/problems/implement-rand10-using-rand7/submissions/
-# The rand7() API is already defined for you.
-# def rand7():
-# @return a random integer in the range 1 to 7
+# Idea: rand7() -> rand49() -> rand40() -> rand10()
 
 class Solution:
     def rand10(self):
         res = 40
         while res >= 40:
-            # Generating random numbers from 1-42 ->  7 * (rand7() - 1)
+            # Generating random numbers from 1-49 ->  7 * (rand7() - 1)
             res = 7 * (rand7() - 1) + (rand7() - 1)
         # +1 to generate 10 as random number
         return res % 10 + 1
