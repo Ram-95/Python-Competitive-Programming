bc215ab2bad8ca49b06e3ba74e785ae422bd08d9
Ram Babu <ram.kottapally@gmail.com>
2020-08-31 16:18:36 +0530

    Create Day-31_Delete_Node_in_BST.py

---
+++ Leetcode_30day_challenge/August_Challenge_2020/Day-31_Delete_Node_in_BST.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root, key):
+        """
+        :type root: TreeNode
+        :type key: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return
+        
+        # we always want to delete the node when it is the root of a subtree,
+        # so we handle left or right according to the val.
+        # if the node does not exist, we will hit the very first if statement and return None.
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+            
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        
+        # now the key is the root of a subtree
+        else:
+            # if the subtree does not have a left child, we just return its right child
+            # to its father, and they will be connected on the higher level recursion.
+            if not root.left:
+                return root.right
+            
+            # if it has a left child, we want to find the max val on the left subtree to 
+            # replace the node we want to delete.
+            else:
+                # try to find the max value on the left subtree
+                tmp = root.left
+                while tmp.right:
+                    tmp = tmp.right
+                    
+                # replace
+                root.val = tmp.val
+                
+                # since we have replaced the node we want to delete with the tmp, now we don't
+                # want to keep the tmp on this tree, so we just use our function to delete it.
+                # pass the val of tmp to the left subtree and repeat the whole approach.
+                root.left = self.deleteNode(root.left, tmp.val)
+        
+        return root
+        
