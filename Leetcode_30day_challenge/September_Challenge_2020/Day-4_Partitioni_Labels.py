b7013bf9884266c9a0617eb6ac569d6181356084
Ram Babu <ram.kottapally@gmail.com>
2020-09-04 13:44:26 +0530

    Create Day-4_Partitioni_Labels.py

---
+++ Leetcode_30day_challenge/September_Challenge_2020/Day-4_Partitioni_Labels.py
@@ -0,0 +1,31 @@
+''' My Solution - O(N) Time and Space
+https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/554/week-1-september-1st-september-7th/3448/discuss/828005/Python-O(N)-Dictionary-and-Set
+'''
+class Solution:
+    def partitionLabels(self, S: str) -> List[int]:
+        # Stores the frequency of the characters of S
+        d = collections.Counter(S)
+        ans = []
+        # Stores the characters
+        present = set()
+        temp = ''
+        
+        def check(present, d):
+            '''Checks if all the contents of 'present' set are exhausted. i.e frequency is Zero'''
+            for i in present:
+                if d[i] > 0:
+                    return False
+            return True
+            
+        for i in S:
+            present.add(i)
+            temp += i
+            d[i] -= 1
+            # Check if the frequency of contents of 'present' set is exhausted - If True, append the length of 
+            # temp to ans and reset temp to ''
+            if check(present, d):
+                ans.append(len(temp))
+                temp = ''
+        
+        return ans
+  
