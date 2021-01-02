5ab41c7081f122f4673c83c4d9b5adf9a1c17b4d
Ram Babu <ram.kottapally@gmail.com>
2021-01-02 14:58:33 +0530

    Create Day-2_Find a Corresponding Node of a Binary Tree in a Clone of That Tree.py

---
+++ Leetcode_30day_challenge/January_Challenge_2021/Day-2_Find a Corresponding Node of a Binary Tree in a Clone of That Tree.py
@@ -0,0 +1,14 @@
+class Solution:
+    ans = None
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        self.inorder(cloned, target)
+        return self.ans
+
+    def inorder(self, root, target):
+        if root is None:
+            return
+        self.inorder(root.left, target)
+        if root.val == target.val:
+            self.ans = root
+            #print(f'Found: {root}')
+        self.inorder(root.right, target)
