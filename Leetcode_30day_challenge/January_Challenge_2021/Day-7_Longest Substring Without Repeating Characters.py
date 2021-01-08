c8f41540e54e93728b21f33a61b3eef5956c2025
Ram Babu <ram.kottapally@gmail.com>
2021-01-08 13:32:28 +0530

    Update Day-7_Longest Substring Without Repeating Characters.py

--- Leetcode_30day_challenge/January_Challenge_2021/Day-7_Longest Substring Without Repeating Characters.py
+++ Leetcode_30day_challenge/January_Challenge_2021/Day-7_Longest Substring Without Repeating Characters.py
@@ -1,3 +1,4 @@
+'''My Solution using deque.'''
 from collections import deque
 
 class Solution:
@@ -24,3 +25,21 @@ class Solution:
         return ans
         
         
+'''Optimized solution of above. Instead of using a deque and popping elements one-by-one, we can directly calculate the answer.'''
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        ans = 0
+        # mp stores the current index of a character
+        mp = {}
+
+        i = 0
+        # try to extend the range [i, j]
+        for j in range(n):
+            if s[j] in mp:
+                i = max(mp[s[j]], i)
+
+            ans = max(ans, j - i + 1)
+            mp[s[j]] = j + 1
+
+        return ans
