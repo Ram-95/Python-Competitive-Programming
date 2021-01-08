1587ba600154ba67d83a66e684517641906ebe5a
Ram Babu <ram.kottapally@gmail.com>
2021-01-08 13:35:06 +0530

    Create Day-8_Check If Two String Arrays are Equivalent.py

---
+++ Leetcode_30day_challenge/January_Challenge_2021/Day-8_Check If Two String Arrays are Equivalent.py
@@ -0,0 +1,6 @@
+'''Straight forward Python solution.'''
+
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        return ''.join(word1) == ''.join(word2)
+        
