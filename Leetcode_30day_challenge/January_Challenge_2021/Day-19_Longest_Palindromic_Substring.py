a45256e2246e2681853fc56a9120bf3e0072ca35
Ram Babu <ram.kottapally@gmail.com>
2021-01-19 14:18:02 +0530

    Update Day-19_Longest_Palindromic_Substring.py

--- Leetcode_30day_challenge/January_Challenge_2021/Day-19_Longest_Palindromic_Substring.py
+++ Leetcode_30day_challenge/January_Challenge_2021/Day-19_Longest_Palindromic_Substring.py
@@ -1,21 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ""
+        ans = ""
         for i in range(len(s)):
-            # odd case, like "aba"
-            tmp = self.helper(s, i, i)
-            if len(tmp) > len(res):
-                res = tmp
-            # even case, like "abba"
-            tmp = self.helper(s, i, i+1)
-            if len(tmp) > len(res):
-                res = tmp
-        return res
-
-    # get the longest palindrome, l, r are the middle indexes   
-    # from inner to outer
-    def helper(self, s, l, r):
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            l -= 1; r += 1
-        return s[l+1:r]
+            # Odd Length substring
+            temp = self.helper(s, i, i)
+            if len(temp) > len(ans):
+                ans = temp
+            
+            # Even length substring
+            temp = self.helper(s, i, i+1)
+            if len(temp) > len(ans):
+                ans = temp
+            
+        return ans
+    
+    # Helper function to grow the substring two ways until no more palindrome is found
+    def helper(self, sub_str, left, right):
+        while left >= 0 and right < len(sub_str) and sub_str[left] == sub_str[right]:
+            left -= 1
+            right += 1
+        
+        return sub_str[left+1:right]
         
