670d63a3f750ef0582d680e699ba6a53b133ddc6
Ram Babu <ram.kottapally@gmail.com>
2021-01-19 14:04:30 +0530

    Create Day-19_Longest_Palindromic_Substring.py

---
+++ Leetcode_30day_challenge/January_Challenge_2021/Day-19_Longest_Palindromic_Substring.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        for i in range(len(s)):
+            # odd case, like "aba"
+            tmp = self.helper(s, i, i)
+            if len(tmp) > len(res):
+                res = tmp
+            # even case, like "abba"
+            tmp = self.helper(s, i, i+1)
+            if len(tmp) > len(res):
+                res = tmp
+        return res
+
+    # get the longest palindrome, l, r are the middle indexes   
+    # from inner to outer
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1; r += 1
+        return s[l+1:r]
+        
