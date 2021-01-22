bf5063ca95f012623e158995ca4e36dcab250456
Ram Babu <ram.kottapally@gmail.com>
2021-01-22 19:15:12 +0530

    Update Day-22_Determine_if_two_strings_are_close.py

--- Leetcode_30day_challenge/January_Challenge_2021/Day-22_Determine_if_two_strings_are_close.py
+++ Leetcode_30day_challenge/January_Challenge_2021/Day-22_Determine_if_two_strings_are_close.py
@@ -25,3 +25,24 @@ class Solution:
         
         return keys1 == keys2 and val1 == val2
         
+
+'''My Solution O(N) Time and Space.'''
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        count1 = [0] * 26
+        count2 = [0] * 26
+        a = ord('a')
+        s1 = set(word1)
+        s2 = set(word2)
+        
+        for i in word1:
+            count1[ord(i) - a] += 1
+        
+        for i in word2:
+            count2[ord(i) - a] += 1
+         
+        count1.sort()
+        count2.sort()
+            
+        return count1 == count2 and s1 == s2
+        
