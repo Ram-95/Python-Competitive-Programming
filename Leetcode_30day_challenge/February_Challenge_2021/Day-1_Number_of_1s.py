865fceb76e134f0641b1dcec6603d4dd01253506
Ram Babu <ram.kottapally@gmail.com>
2021-02-01 15:36:38 +0530

    Create Day-1_Number_of_1s.py

---
+++ Leetcode_30day_challenge/February_Challenge_2021/Day-1_Number_of_1s.py
@@ -0,0 +1,9 @@
+''' My Solution: O(log N) Time and O(1) Space '''
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        ans = 0
+        while n:
+            ans += n & 1
+            n >>= 1
+        return ans
