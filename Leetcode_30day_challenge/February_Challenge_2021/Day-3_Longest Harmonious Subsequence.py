44c4cf672c144050e7157a256f62206bfdeedcbc
Ram Babu <ram.kottapally@gmail.com>
2021-02-04 14:25:15 +0530

    Create Day-3_Longest Harmonious Subsequence.py

---
+++ Leetcode_30day_challenge/February_Challenge_2021/Day-3_Longest Harmonious Subsequence.py
@@ -0,0 +1,27 @@
+'''My Solution - O(N) Time and Space.'''
+
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        ans = 0
+        d = {}
+		# Putting the items of nums in dictionary with their frequencies
+        for i in nums:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        # Iterating over the keys of dictionary
+        for i in d.keys():
+			# Find out the numbers that differ from the current number by 1
+            a,b = i-1, i+1
+			# Stores the current answer - Possible answer in the current iteration
+            curr = 0
+			# Update 'curr' only if the dictionary has either 'a' or 'b' in it and their frequency is greater than 0
+            if d.get(a, -1) > 0:
+                curr += d[a] + d[i]
+            elif d.get(b, -1) > 0:
+                curr += d[b] + d[i]
+			# Update the maximum answer possible
+            ans = max(ans, curr)
+            
+        return ans 
