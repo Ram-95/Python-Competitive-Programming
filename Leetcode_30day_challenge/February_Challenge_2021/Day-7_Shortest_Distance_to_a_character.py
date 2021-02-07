39493cf48eb52c196317884470326bdebe2ce1fd
Ram Babu <ram.kottapally@gmail.com>
2021-02-07 14:18:26 +0530

    Update Day-7_Shortest_Distance_to_a_character.py

--- Leetcode_30day_challenge/February_Challenge_2021/Day-7_Shortest_Distance_to_a_character.py
+++ Leetcode_30day_challenge/February_Challenge_2021/Day-7_Shortest_Distance_to_a_character.py
@@ -1,3 +1,5 @@
+'''My Solution - O(N * set size) Time and O(set size) Space.'''
+
 class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
         m = set()
@@ -18,4 +20,9 @@ class Solution:
                 
         return ans
             
-        
+'''
+O(N) Solution using Min array of indices.
+1. Traverse left to right and fill the array with difference of indices.
+2. Traverse right to left and fill the array with difference of indices.
+3. Ans is minimum indices of above.
+'''     
