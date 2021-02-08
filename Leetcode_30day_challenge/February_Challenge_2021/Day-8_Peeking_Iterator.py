19e263c1d2fc8c1b99f2c90b4a6c68079d92c9a9
Ram Babu <ram.kottapally@gmail.com>
2021-02-08 14:35:48 +0530

    Update Day-8_Peeking_Iterator.py

--- Leetcode_30day_challenge/February_Challenge_2021/Day-8_Peeking_Iterator.py
+++ Leetcode_30day_challenge/February_Challenge_2021/Day-8_Peeking_Iterator.py
@@ -19,6 +19,7 @@
 #         :rtype: int
 #         """
 
+'''*************************************************************** My Implementation - Using extra Space ****************************************************************'''
 class PeekingIterator:
     def __init__(self, iterator):
         """
@@ -58,9 +59,43 @@ class PeekingIterator:
         """
         #self.curr += 1
         return self.size > self.curr
-        
+ 
+
+'''*************************************************** My Implementation - Without extra Space. ************************************************************'''
+class PeekingIterator:
+    def __init__(self, iterator):
+        """
+        Initialize your data structure here.
+        :type iterator: Iterator
+        """
+        self.t = iterator
+        self.val = self.t.next() if self.t.hasNext() else None
+
+    def peek(self):
+        """
+        Returns the next element in the iteration without advancing the iterator.
+        :rtype: int
+        """
+        return self.val
         
 
+    def next(self):
+        """
+        :rtype: int
+        """
+        e = self.val
+        self.val = self.t.next() if self.t.hasNext() else None
+        return e
+        
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return self.val is not None
+
+    
+ 
 # Your PeekingIterator object will be instantiated and called as such:
 # iter = PeekingIterator(Iterator(nums))
 # while iter.hasNext():
