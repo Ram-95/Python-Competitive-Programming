08a510342ad5c78e4c5359b665e98289c93b25ed
Ram Babu <ram.kottapally@gmail.com>
2021-02-23 14:28:53 +0530

    Update Day-23_Search_a_2D_Matrix.py

--- Leetcode_30day_challenge/February_Challenge_2021/Day-23_Search_a_2D_Matrix.py
+++ Leetcode_30day_challenge/February_Challenge_2021/Day-23_Search_a_2D_Matrix.py
@@ -8,4 +8,25 @@ class Solution:
                 if matrix[i][j] == target:
                     return True
         return False
+
+''' Linear Search & Binary Search - O(Rows + log(Cols))''' 
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        for i in range(rows):
+            if target in range(matrix[i][0], matrix[i][-1]+1):
+                row = matrix[i]
+                #Binary Search Code
+                low, high = 0, cols-1
+                while low <= high:
+                    mid = (low + high)//2
+                    if row[mid] == target:
+                        return True
+                    elif row[mid] < target:
+                        low = mid + 1
+                    else:
+                        high = mid - 1
+        
+        return False
         
