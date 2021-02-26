1782e18cf9117e4991cedcc5e6f1f4cd4981c819
Ram Babu <ram.kottapally@gmail.com>
2021-02-26 14:44:12 +0530

    Update Day-27_Validate_Stack_Sequences.py

--- Leetcode_30day_challenge/February_Challenge_2021/Day-27_Validate_Stack_Sequences.py
+++ Leetcode_30day_challenge/February_Challenge_2021/Day-27_Validate_Stack_Sequences.py
@@ -5,22 +5,22 @@ class Solution:
         n = len(pushed)
         if n == 0:
             return True
-		    # p1 - points to pushed list; p2 - points to popped list
+		# p1 - points to pushed list; p2 - points to popped list
         p1,p2 = 0,0
         st = []
         while p1 < n and p2 < n:
-			  # Push items to stack until the items of 'pushed[p1]' and 'popped[p2]' doesn't match
+			# Push items to stack until the items of 'pushed[p1]' and 'popped[p2]' doesn't match
             if pushed[p1] != popped[p2]:
                 st.append(pushed[p1])
                 p1 += 1
             else:
                 p1 += 1
                 p2 += 1
-			      # Keep popping items from stack(st) if top of 'st' and popped[p2] are same
+			# Keep popping items from stack(st) if top of 'st' and popped[p2] are same
             while st and st[-1] == popped[p2]:
                 st.pop()
                 p2 += 1
         
-		    # Finally if 'st' is empty, then it's a VALID sequence else NOT VALID sequence
+		# Finally if 'st' is empty, then it's a VALID sequence else NOT VALID sequence
         return st == []
      
