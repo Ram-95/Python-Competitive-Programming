56d185008b0ce3f645e2f2f51101d9a542404963
Ram Babu <ram.kottapally@gmail.com>
2021-03-07 15:45:02 +0530

    Create Day-6_Short_Encoding_of_Words.py

---
+++ Leetcode_30day_challenge/March_Challenge_2021/Day-6_Short_Encoding_of_Words.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/short-encoding-of-words/solution/
+class Solution(object):
+    def minimumLengthEncoding(self, words):
+        good = set(words)
+        for word in words:
+            for k in range(1, len(word)):
+                good.discard(word[k:])
+
+        return sum(len(word) + 1 for word in good)
