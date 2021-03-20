7f80a7cba8dcd4147f48801bfafad23611d370fe
Ram Babu <ram.kottapally@gmail.com>
2021-03-20 14:12:53 +0530

    Create Day-19_Keys_and_Rooms.py

---
+++ Leetcode_30day_challenge/March_Challenge_2021/Day-19_Keys_and_Rooms.py
@@ -0,0 +1,20 @@
+''' My Solution - DFS | O(E+V) Time '''
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+      # Starting room
+        start = [0]
+      # To know if a room has been visited or not
+        visited = set()
+      # Adding the first room to set
+        visited.add(0)
+        while start:
+            temp = start.pop()
+            for i in rooms[temp]:
+              # If a room is not visited, add it to the start and mark it as visited
+                if i not in visited:
+                    start.append(i)
+                    visited.add(i)
+        
+        # If every room is visited return True else return False
+        return len(visited) == len(rooms)
