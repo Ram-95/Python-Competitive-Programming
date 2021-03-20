3fcedac6d306a12a39aea5da2acfc4bf718cdf29
Ram Babu <ram.kottapally@gmail.com>
2021-03-20 14:16:28 +0530

    Update Day-19_Keys_and_Rooms.py

--- Leetcode_30day_challenge/March_Challenge_2021/Day-19_Keys_and_Rooms.py
+++ Leetcode_30day_challenge/March_Challenge_2021/Day-19_Keys_and_Rooms.py
@@ -18,3 +18,27 @@ class Solution:
         
         # If every room is visited return True else return False
         return len(visited) == len(rooms)
+
+''' Minor Optimization '''
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+      # Starting room
+        start = [0]
+      # To know if a room has been visited or not
+        visited = set()
+      # Adding the first room to set
+        visited.add(0)
+        while start:
+            temp = start.pop()
+            for i in rooms[temp]:
+              # If a room is not visited, add it to the start and mark it as visited
+                if i not in visited:
+                    start.append(i)
+                    visited.add(i)
+            # Avoids unnecessary looping in some cases
+            if len(visited) == len(rooms):
+               return True
+          
+        return False
+        
+        
