59011f105df5dd609e07eb7d61a5c77758669869
Ram Babu <ram.kottapally@gmail.com>
2021-03-27 14:08:54 +0530

    Create Day-27_Palindromic_Substring.py

---
+++ Leetcode_30day_challenge/March_Challenge_2021/Day-27_Palindromic_Substring.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        ans = 0
+        n = len(s)
+        # Base case
+        if n == 0:
+            return 0
+        
+        def solve(s, left, right):
+            count = 0
+            while left >= 0 and right < n and s[left] == s[right]:
+                count += 1
+                # Extending the string from mid to the left. i.e, why Decrease left 
+                left -= 1
+                # Extending the string from mid to the right. i.e, why Increase right 
+                right += 1
+            return count
+        
+        for mid in range(n):
+            # Finding the palindrome of Odd length strings
+            ans += solve(s, mid, mid)
+            
+            # Finding the palindrome of Even length strings
+            ans += solve(s, mid, mid + 1)
+        
+        return ans
+        
