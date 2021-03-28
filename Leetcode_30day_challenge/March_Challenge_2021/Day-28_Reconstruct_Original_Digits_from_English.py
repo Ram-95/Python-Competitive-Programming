590076217a883c621c86385f48359d1750c92a5e
Ram Babu <ram.kottapally@gmail.com>
2021-03-28 14:42:52 +0530

    Update Day-28_Reconstruct_Original_Digits_from_English.py

--- Leetcode_30day_challenge/March_Challenge_2021/Day-28_Reconstruct_Original_Digits_from_English.py
+++ Leetcode_30day_challenge/March_Challenge_2021/Day-28_Reconstruct_Original_Digits_from_English.py
@@ -1,3 +1,20 @@
+'''
+Solutions Based on the following fact: 
+----------------------------------------
+The even digits all have a unique letter while the odd digits all don't:
+
+zero: Only digit with z
+two: Only digit with w
+four: Only digit with u
+six: Only digit with x
+eight: Only digit with g
+
+The odd ones for easy looking, each one's letters all also appear in other digit words:
+one, three, five, seven, nine
+
+'''
+
+''' Solution - 1 '''
 class Solution:
     def originalDigits(self, s):
         d = collections.Counter(s)
@@ -8,3 +25,23 @@ class Solution:
                 d[c] -= d[x[-1]]
         return ''.join(sorted(res))
         
+
+''' Solution - 2 '''
+class Solution:
+    def originalDigits(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = ""
+        res += "0"*s.count('z')
+        res += "1"*(s.count('o')-s.count('z')-s.count('w')-s.count('u'))
+        res += "2"*s.count('w')
+        res += "3"*(s.count('h') - s.count('g'))
+        res += "4"*s.count('u')
+        res += "5"*(s.count('f') - s.count('u'))
+        res += "6"*s.count('x')
+        res += "7"*(s.count('s')-s.count('x'))
+        res += "8"*s.count("g")
+        res += "9"*(s.count('i') - s.count('x') - s.count("g") - s.count('f') + s.count('u'))
+        return res
