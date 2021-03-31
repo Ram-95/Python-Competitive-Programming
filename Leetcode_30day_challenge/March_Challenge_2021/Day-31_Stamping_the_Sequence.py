c4cc1431a14fb932092987c80e17f0bffd133112
Ram Babu <ram.kottapally@gmail.com>
2021-03-31 13:13:32 +0530

    Create Day-31_Stamping_the_Sequence.py

---
+++ Leetcode_30day_challenge/March_Challenge_2021/Day-31_Stamping_the_Sequence.py
@@ -0,0 +1,21 @@
+class Solution:
+    def movesToStamp(self, s, t):
+        n, m, t, s, res = len(t), len(s), list(t), list(s), []
+
+        def check(i):
+            changed = False
+            for j in range(m):
+                if t[i + j] == '?': continue
+                if t[i + j] != s[j]: return False
+                changed = True
+            if changed:
+                t[i:i + m] = ['?'] * m
+                res.append(i)
+            return changed
+
+        changed = True
+        while changed:
+            changed = False
+            for i in range(n - m + 1):
+                changed |= check(i)
+        return res[::-1] if t == ['?'] * n else []
