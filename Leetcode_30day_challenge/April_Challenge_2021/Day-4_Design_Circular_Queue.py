4f2ce155266551421d09b25b5cbfb0e6dc7d4146
Ram Babu <ram.kottapally@gmail.com>
2021-04-04 13:46:29 +0530

    Update Day-4_Design_Circular_Queue.py

--- Leetcode_30day_challenge/April_Challenge_2021/Day-4_Design_Circular_Queue.py
+++ Leetcode_30day_challenge/April_Challenge_2021/Day-4_Design_Circular_Queue.py
@@ -1,4 +1,4 @@
-# Using inbuilt Deque
+# Solution - 1 | Using inbuilt Deque
 from collections import deque
 
 class MyCircularQueue:
@@ -41,3 +41,47 @@ class MyCircularQueue:
     def isFull(self) -> bool:
         return len(self.d) == self.k
         
+
+#################################################################################################################################################
+
+# Solution - 2 | Using list
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.q = []
+        self.k = k
+        
+
+    def enQueue(self, value: int) -> bool:
+        if len(self.q) < self.k:
+            self.q.append(value)
+            return True
+        return False
+        
+
+    def deQueue(self) -> bool:
+        if len(self.q) > 0:
+            self.q.pop(0)
+            return True
+        return False
+        
+
+    def Front(self) -> int:
+        if len(self.q) > 0:
+            return self.q[0]
+        return -1
+        
+
+    def Rear(self) -> int:
+        if len(self.q) > 0:
+            return self.q[-1]
+        return -1
+        
+
+    def isEmpty(self) -> bool:
+        return len(self.q) == 0
+        
+
+    def isFull(self) -> bool:
+        return len(self.q) == self.k
+
