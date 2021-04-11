7a66c639ded62b46c4d784e6c2b1b423b5f67098
Ram Babu <ram.kottapally@gmail.com>
2021-04-11 16:22:51 +0530

    Update Day-11_Deepest_Leaves_Sum.py

--- Leetcode_30day_challenge/April_Challenge_2021/Day-11_Deepest_Leaves_Sum.py
+++ Leetcode_30day_challenge/April_Challenge_2021/Day-11_Deepest_Leaves_Sum.py
@@ -28,3 +28,32 @@ class Solution:
         sum_deep_leaves(root, 0, 0, x-1)
         
         return self.res
+
+    
+# Solution - 2: Using Queue | O(N) Time and Space.
+class Solution:
+    def deepestLeavesSum(self, root: TreeNode) -> int:
+        ans = 0
+        # Base Condition
+        if root is None:
+            return 0
+        
+        q = [root]
+        # Keep adding the child level after level
+        while True:
+            # Add the left child
+            q_new = [x.left for x in q if x.left]
+            # Add the right child
+            q_new += [x.right for x in q if x.right]
+            # If q_new is empty, that means we have reached the end of all levels of tree
+            if not q_new:
+                break
+            # Make the current level as previous
+            q = q_new
+        
+        # Sum up all the values of the last level and return the answer
+        for i in q:
+            ans += i.val
+        return ans
+        
+        
