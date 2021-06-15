153652dd88b883717a39ee98ca11883edd574687
Ram Babu <ram.kottapally@gmail.com>
2021-06-15 19:05:40 +0530

    Create Day-15_Matchsticks_to_Square.py

---
+++ Leetcode_30day_challenge/June_Challenge_2021/Day-15_Matchsticks_to_Square.py
@@ -0,0 +1,20 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        k = 4
+        sums = [0] * k
+        subsum = sum(matchsticks)/k
+        matchsticks.sort(reverse=True)
+        
+        def solve(i):
+            if i == len(matchsticks):
+                return len(set(sums)) == 1
+            for j in range(k):
+                sums[j] += matchsticks[i]
+                if sums[j] <= subsum and solve(i+1):
+                    return True
+                sums[j] -= matchsticks[i]
+                if sums[j] == 0:
+                    break
+            return False
+        
+        return solve(0)
