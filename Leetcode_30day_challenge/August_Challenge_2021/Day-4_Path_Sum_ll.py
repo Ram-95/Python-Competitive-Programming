ebd284278accd2cdff420e73e3210a702cb57dec
Ram Babu <ram.kottapally@gmail.com>
2021-08-04 17:11:43 +0530

    Update Day-4_Path_Sum_ll.py

--- Leetcode_30day_challenge/August_Challenge_2021/Day-4_Path_Sum_ll.py
+++ Leetcode_30day_challenge/August_Challenge_2021/Day-4_Path_Sum_ll.py
@@ -1,7 +1,8 @@
 """
 Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where each path's sum equals targetSum.
 
-Solution: O(N) Time and O(Height) Space.
+Solution - 1: O(N) Time and O(Height) Space. 
+Find the path, sum the path and check if pathsum equals targetSum
 """
 
 # Definition for a binary tree node.
@@ -39,3 +40,33 @@ class Solution:
         get_sum(root, [], targetSum, res)
         return res
         
+"""
+Solution-2: O(N) Time and O(Height) Space
+Calculate the Sum on the go while traversing the path from root to leaf
+"""
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+        def get_sum(root, ans, sums, res):
+            if root is None:
+                return res
+            
+            # Appending the root value to arr
+            ans.append(root.val)
+            
+            # If root is leaf node and sums == root.val (i.e we have found a path whose sum equals targetSum), print the path
+            if root.left is None and root.right is None and sums == root.val:
+                res.append(list(ans))
+                    
+            sums -= root.val
+            # Recur on the LST
+            get_sum(root.left, ans, sums, res)
+            # Recur on the RST
+            get_sum(root.right, ans, sums, res)
+            
+            # Pop off the last element, as we are moving from LST to RST and viceversa
+            ans.pop()
+            
+         
+        res = []
+        get_sum(root, [], targetSum, res)
+        return res
