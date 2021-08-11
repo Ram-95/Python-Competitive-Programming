f7388daa50cf9d95a6b572224765ae30a1ab10de
Ram Babu <ram.kottapally@gmail.com>
2021-08-11 17:03:32 +0530

    Update Day-11_Array of Doubled Pairs.py

--- Leetcode_30day_challenge/August_Challenge_2021/Day-11_Array of Doubled Pairs.py
+++ Leetcode_30day_challenge/August_Challenge_2021/Day-11_Array of Doubled Pairs.py
@@ -6,10 +6,13 @@ class Solution:
     
         for i in arr:
             double = 2*i
+            # Since we have sorted the array, we'll first iterate over the small values and if small values frequency is zero, we just continue
             if d[i] == 0:
                 continue
+            # If double values frequency is Zero, that means there is no double value for i. So we return False    
             if d[double] == 0:
                 return False
+            # Decrement both i and double
             d[double] -= 1
             d[i] -= 1
             
