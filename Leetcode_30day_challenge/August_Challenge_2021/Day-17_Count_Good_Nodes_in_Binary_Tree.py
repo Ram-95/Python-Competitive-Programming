8c5860d882d7de795af0f6cea7210bd1891fe052
Ram Babu <ram.kottapally@gmail.com>
2021-08-17 15:15:16 +0530

    Update Day-17_Count_Good_Nodes_in_Binary_Tree.py

--- Leetcode_30day_challenge/August_Challenge_2021/Day-17_Count_Good_Nodes_in_Binary_Tree.py
+++ Leetcode_30day_challenge/August_Challenge_2021/Day-17_Count_Good_Nodes_in_Binary_Tree.py
@@ -1,4 +1,4 @@
-# O(N) Time and Space.
+# O(N) Time and Space - Storing the Good Nodes and count them at last.
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         
@@ -15,3 +15,16 @@ class Solution:
         ans = []
         dfs(root, ans, float('-inf'))
         return len(ans)
+
+# O(N) Time and O(H) Space. - No list used
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        
+        def dfs(root: TreeNode, ans: list, maxm: float) -> int:
+            if root is None:
+                return 0          
+            maxm = max(root.val, maxm)
+            return int(root.val >= maxm) + dfs(root.left, ans, maxm) + dfs(root.right, ans, maxm)
+        
+        ans = 0
+        return dfs(root, ans, root.val)
