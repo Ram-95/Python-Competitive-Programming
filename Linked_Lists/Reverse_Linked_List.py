28b2a11c9916d43e87e19c10f99fce64f8f5d9d9
Ram Babu <ram.kottapally@gmail.com>
2021-10-18 13:29:25 +0530

    Create Reverse_Linked_List.py

---
+++ Linked_Lists/Reverse_Linked_List.py
@@ -0,0 +1,12 @@
+# Iterative | Time: O(N) & Space: O(1)
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        while head:
+            x = head.next
+            head.next = prev
+            prev = head
+            head = x
+        
+        return prev
