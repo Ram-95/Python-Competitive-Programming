8dfbf1f844b387eaa712d8545e797a0dcf5fd149
Ram Babu <ram.kottapally@gmail.com>
2021-11-27 13:26:12 +0530

    Update Swap_Nodes_in_Pairs.py

--- Linked_Lists/Swap_Nodes_in_Pairs.py
+++ Linked_Lists/Swap_Nodes_in_Pairs.py
@@ -1,4 +1,4 @@
-# Solution - 1: Time: O(N) and Space: O(1)
+# Solution - 1: Swapping the Nodes | Time: O(N) and Space: O(1)
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # Base condition
@@ -21,3 +21,25 @@ class Solution:
                 p2 = p1.next
             
         return t_head
+
+    
+# Solution - 2: Swapping the Values | Time: O(N) and Space: O(1)
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        p1, p2 = head, head.next
+        t_head = head
+        
+        while p1 and p2:
+            temp = p2.val
+            p2.val = p1.val
+            p1.val = temp
+            
+            if p2.next and p2.next.next:
+                p1 = p2.next
+                p2 = p1.next
+            else:
+                break
+
+        return head
